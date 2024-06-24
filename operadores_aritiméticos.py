cd3324603fc776a7f8aaa2a5437b96082d4f99d4
Lidia Valle <lidiavale88@gmail.com>
2024-06-24 20:03:01 -0300

    Create operadores_aritiméticos.py

---
+++ operadores_aritiméticos.py
@@ -0,0 +1,8 @@
+#operadores basicos python
+produto_1 = 20
+produto_2 = 5
+print(produto_1 + produto_2)
+print(produto_1 - produto_2)
+print(produto_1 * produto_2)
+print(produto_1 / produto_2)
+print(produto_1 // produto_2)
