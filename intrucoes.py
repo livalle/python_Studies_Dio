0f8bff19e54ed9d49d392949cf7fa0d121dfbc12
Lidia Valle <lidiavale88@gmail.com>
2024-06-25 11:41:31 -0300

    Create intrucoes.py
    Um pouco de pratica em if, else, e outros

---
+++ intrucoes.py
@@ -0,0 +1,6 @@
+age_1 = int (input("Digit your age"))
+age = age_1
+if age < 18:
+ print(f"You are {age_1} years old! go home now!!")
+elif age >18:
+ print(f"You are {age_1}, so you can join our beer party!")
